08dd42d3084206b7db30dfa0b2b07f5a8ae527e8
nitin bhatia <nitinbhatia6839@gmail.com>
2023-05-18 18:14:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import ThreeSixty from "react-360-view";
+
+function App() {
+  return (
+    <div className="maindiv">
+      <div className="App">
+        <ThreeSixty
+          amount={70}
+          imagePath=""
+          fileName="images/body-{index}.jpeg"
+        />
+      </div>
+      <span className="hotspot one"></span>
+      <span className="hotspot two"></span>
+      <span className="hotspot three"></span>
+      <span className="hotspot four"></span>
+      <span className="hotspot five"></span>
+    </div>
+  );
+}
+
+export default App;
